2b96883df9c3609c6977659378382e4500f24662
Brian Preston <brian8181@gmail.com>
2020-04-29 05:51:40 -0500

    test test cases

--- src/unit_test.cpp
+++ src/unit_test.cpp
@@ -1,12 +1,36 @@
 #include "catch.hpp"
-//#include "utility.h"
+#include "utility.h"
 #include <string>
 
 unsigned int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
 }
 
-void replace_all(std::string& s, const std::string& sub_str, const std::string& replace_str);
+//void replace_all(std::string& s, const std::string& sub_str, const std::string& replace_str);
+
+TEST_CASE("Test1")
+{
+    REQUIRE( 1==1 );
+    REQUIRE( 2==2 );
+}
+
+TEST_CASE("Test2")
+{
+    REQUIRE( 1==1 );
+    REQUIRE( 2==2 );
+}
+
+TEST_CASE("Test3")
+{
+    REQUIRE( 1==1 );
+    REQUIRE( 2==2 );
+}
+
+TEST_CASE("Test4")
+{
+    REQUIRE( 1==1 );
+    REQUIRE( 2==2 );
+}
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( Factorial(1) == 1 );
@@ -15,18 +39,17 @@ TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( Factorial(10) == 3628800 );
 }
 
-// TEST_CASE("Test1", "scanpf")void replace_all(string& s, const string& sub_str, const string& rvoid replace_all(string& s, const string& sub_str, const string& replace_str)eplace_str)
+TEST_CASE("Replace All")
+{
+    std::string s = "Hello BEFORE this [BEFORE] <BEFORE> BEFORE";
+    replace_all(s, "BEFORE", "AFTER");
+    REQUIRE( s == "Hello AFTER this [AFTER] <AFTER> AFTER" );
+    REQUIRE_FALSE( s != "Hello AFTER this [AFTER] <AFTER> AFTER" );
+}
+
+// TEST_CASE("Test1", "scanpf")
 // {
     
-// }
-
-
-// TEST_CASE("Replace All")
-// {
-//     const std::string s = "Hello BEFORE this [BEFORE] <BEFORE> BEFORE";
-//     replace_all(s, "BEFORE", "AFTER");
-//     REQUIRE( s == "Hello AFTER this [AFTER] <AFTER> AFTER" );
-//     REQUIRE_FALSE( s != "Hello AFTER this [AFTER] <AFTER> AFTER" );
 // }
 
 // g++ utility.cpp unit_test.cpp -o unit_test
